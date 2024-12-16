b4a79c7579c5ae70a7b0c60e98af804621495f18
ganthegun <ganjunwei.business@gmail.com>
2024-12-16 16:14:01 +0800

    latest update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:sep_hcms/route.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  runApp(
-    MaterialApp.router(
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
       routerConfig: router,
       debugShowCheckedModeBanner: false,
-    ),
-  );
+    );
+  }
 }
--- lib/view/page/booking/booking_form.dart
+++ lib/view/page/booking/booking_form.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:file_picker/file_picker.dart';
 
-class BookingForm extends StatelessWidget {
+class BookingForm extends StatefulWidget {
+  BookingForm({super.key});
+
+  @override
+  State<BookingForm> createState() => _BookingFormState();
+}
+
+class _BookingFormState extends State<BookingForm> {
   final _formKey = GlobalKey<FormState>();
 
-  BookingForm({super.key});
+  double _size = 50.0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,70 +20,106 @@ class BookingForm extends StatelessWidget {
       builder: (BuildContext context) => Container(
         height: MediaQuery.of(context).size.height * 0.75,
         padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
-        child: ListView(
-          children: [
-            const Text(
-              'Add Booking',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              const Text(
+                'Add Booking',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Center(
-              child: Column(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    borderRadius: BorderRadius.circular(20),
-                    splashFactory: InkRipple.splashFactory,
-                    splashColor: Colors.black.withOpacity(0.2),
-                    child: Ink(
-                      width: 90,
-                      height: 90,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 168, 196, 108),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: const Icon(
-                        Icons.upload_sharp,
-                        color: Colors.white,
-                        size: 45,
-                      ),
+              Center(
+                child: InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.circular(20),
+                  splashFactory: InkRipple.splashFactory,
+                  splashColor: Colors.black.withOpacity(0.2),
+                  child: Ink(
+                    width: 90,
+                    height: 90,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 168, 196, 108),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Icon(
+                      Icons.upload_sharp,
+                      color: Colors.white,
+                      size: 45,
                     ),
                   ),
-                  const Text(
-                    'upload image here',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    textAlign: TextAlign.center,
+                ),
+              ),
+              const Text(
+                'upload image here',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Table(
+                border: null,
+                columnWidths: <int, TableColumnWidth>{
+                  0: IntrinsicColumnWidth(),
+                  1: FlexColumnWidth(),
+                },
+                children: <TableRow>[
+                  TableRow(
+                    children: [
+                      TableCell(
+                        verticalAlignment: TableCellVerticalAlignment.middle,
+                        child: Text(
+                          "Address: ",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      TableCell(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Enter your address",
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        verticalAlignment: TableCellVerticalAlignment.middle,
+                        child: Text(
+                          "Size:",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      TableCell(
+                        child: Slider(
+                          value: _size,
+                          min: 0.0,
+                          max: 100.0,
+                          divisions: 10000,
+                          label: _size.toStringAsPrecision(4) + "sqft",
+                          onChanged: (double value) {
+                            setState(() {
+                              _size = value;
+                            });
+                          },
+                        ),
+                      )
+                    ],
                   ),
                 ],
               ),
-            ),
-            FormBuilder(
-              key: _formKey,
-              child: Column(
-                children: [
-                  FormBuilderTextField(
-                    name: 'address',
-                    decoration: const InputDecoration(
-                      labelText: 'Address',
-                      hintText: 'Enter Address',
-                    ),
-                  ),
-                  FormBuilderSlider(
-                    name: 'size',
-                    initialValue: 0,
-                    min: 0,
-                    max: 100,
-                  ),
-                ],
-              ),
-            ),
-          ],
+              // DatePickerDialog(firstDate: firstDate, lastDate: lastDate)
+            ],
+          ),
         ),
       ),
     );
